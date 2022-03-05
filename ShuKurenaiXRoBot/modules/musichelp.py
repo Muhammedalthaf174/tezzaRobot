8df91adb8d3f48020e588b0d1b9870954890ea57
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-03-05 23:05:45 +0530

    Update musichelp.py

--- ShuKurenaiXRoBot/modules/musichelp.py
+++ ShuKurenaiXRoBot/modules/musichelp.py
@@ -1,7 +1,7 @@
 __mod_name__ = "Musicplayer"
 
 __help__ = """
-──「 Music stream 」──              
+──「 Music player 」──              
 
 ❂ /play (query or reply audio) to playing music from yourube or your audio telegram.
 ❂ /playlist (choose option) for playing your personal playlist or group playlist.
@@ -10,10 +10,9 @@ __help__ = """
 
 ❂ /auth - Add to authorized users.
 ❂ /unauth - Canceling or deleting from authorized users list.
-❂ /msettings - To open panel settings on musicplayer.
-❂ /setadmin - To set all command only for admins.
-❂ /setmember - Set all command for all members.
-❂ /vol (range 1-200) - To set volume on voice calls.
-
-NOTE : DON'T SPAMMING AT REQUEST SONG OR VIDEO...
+❂ /skip - Play next song.
+❂ /pause - Pause the currently playing song.
+❂ /resume - Resume the paused song.
+❂ /end - stop the currently playing music.
+❂ /settings - Get settings dashboard.
 """
