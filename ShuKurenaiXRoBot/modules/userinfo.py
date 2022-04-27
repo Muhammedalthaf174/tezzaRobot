b47bda776428ee149fe30fdc8f861a4813bafc53
Mhd Althaf <93602145+Muhammedalthaf174@users.noreply.github.com>
2022-04-27 18:21:08 +0530

    Update userinfo.py

--- ShuKurenaiXRoBot/modules/userinfo.py
+++ ShuKurenaiXRoBot/modules/userinfo.py
@@ -450,7 +450,7 @@ def set_about_me(update: Update, context: CallbackContext):
 def stats(update: Update, context: CallbackContext):
     stats = "<b>╔═━「 Current 𝑻𝒆𝒛𝒛𝒂❥︎ Statistics 」</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    result += "\n<b>╘═━「 Powered By ShuKurenaiSupport」</b>"
+    result += "\n<b>╘═━「 Powered By Tezzasupport」</b>"
     update.effective_message.reply_photo(
         SHU,caption=result, 
         reply_markup=InlineKeyboardMarkup(
